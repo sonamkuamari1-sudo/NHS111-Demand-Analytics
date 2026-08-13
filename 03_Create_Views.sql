a9135724e5d722ba3e0287983f4584196e923d52
sonamkuamari1-sudo <sonamkuamari1@gmail.com>
2026-08-13 20:29:29 +0100

    Update 03_Create_Views.sql

--- 03_Create_Views.sql
+++ 03_Create_Views.sql
@@ -1,25 +1,91 @@
--- Check Organisation view
-SELECT *
-FROM vw_OrganisationDemand
-ORDER BY TotalDemand DESC;
+USE [NHS111_Analytics];
+GO
 
--- Check Region view
-SELECT *
-FROM vw_RegionDemand
-ORDER BY TotalDemand DESC;
+SET ANSI_NULLS ON;
+GO
 
--- Check Contract view
-SELECT *
-FROM vw_ContractDemand
-ORDER BY TotalRecordedValue DESC;
+SET QUOTED_IDENTIFIER ON;
+GO
 
--- Check KPI view
-SELECT *
-FROM vw_KPIDemand
-ORDER BY TotalRecordedValue DESC;
+/* =========================================================
+   View 1: Organisation Demand
+   ========================================================= */
+CREATE OR ALTER VIEW dbo.vw_OrganisationDemand AS
+SELECT
+    ORG_NAME,
+    SUM(VALUE) AS TotalDemand
+FROM dbo.Fact_IUCADC
+WHERE VALUE IS NOT NULL
+GROUP BY ORG_NAME;
+GO
 
--- Check Monthly view
-SELECT *
-FROM vw_MonthlyDemand
-ORDER BY ReportingDate;
+
+/* =========================================================
+   View 2: Region Demand
+   ========================================================= */
+CREATE OR ALTER VIEW dbo.vw_RegionDemand AS
+SELECT
+    REGION_NAME,
+    SUM(VALUE) AS TotalDemand
+FROM dbo.Fact_IUCADC
+WHERE VALUE IS NOT NULL
+GROUP BY REGION_NAME;
+GO
+
+
+/* =========================================================
+   View 3: Contract Demand
+   ========================================================= */
+CREATE OR ALTER VIEW dbo.vw_ContractDemand AS
+SELECT
+    CONTRACT_NAME,
+    SUM(VALUE) AS TotalRecordedValue
+FROM dbo.Fact_IUCADC
+WHERE VALUE IS NOT NULL
+GROUP BY CONTRACT_NAME;
+GO
+
+
+/* =========================================================
+   View 4: KPI Demand
+   ========================================================= */
+CREATE OR ALTER VIEW dbo.vw_KPIDemand AS
+SELECT
+    ITEM_NUMBER,
+    SUM(VALUE) AS TotalRecordedValue
+FROM dbo.Fact_IUCADC
+WHERE VALUE IS NOT NULL
+GROUP BY ITEM_NUMBER;
+GO
+
+
+/* =========================================================
+   View 5: Monthly Demand
+   ========================================================= */
+CREATE OR ALTER VIEW dbo.vw_MonthlyDemand AS
+SELECT
+    REPORTING_PERIOD,
+    DATEFROMPARTS(
+        RIGHT(REPORTING_PERIOD, 4),
+        CASE SUBSTRING(REPORTING_PERIOD, 16, 3)
+            WHEN 'JAN' THEN 1
+            WHEN 'FEB' THEN 2
+            WHEN 'MAR' THEN 3
+            WHEN 'APR' THEN 4
+            WHEN 'MAY' THEN 5
+            WHEN 'JUN' THEN 6
+            WHEN 'JUL' THEN 7
+            WHEN 'AUG' THEN 8
+            WHEN 'SEP' THEN 9
+            WHEN 'OCT' THEN 10
+            WHEN 'NOV' THEN 11
+            WHEN 'DEC' THEN 12
+        END,
+        1
+    ) AS ReportingDate,
+    SUM(VALUE) AS MonthlyDemand
+FROM dbo.Fact_IUCADC
+WHERE VALUE IS NOT NULL
+GROUP BY REPORTING_PERIOD;
+GO
 
