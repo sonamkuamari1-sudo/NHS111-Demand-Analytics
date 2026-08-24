e9ff95213117e4f096c0219daec1d8538afae2f9
sonamkuamari1-sudo <sonamkuamari1@gmail.com>
2026-08-24 17:15:47 +0100

    Update 02_Business_Analysis.sql

--- 02_Business_Analysis.sql
+++ 02_Business_Analysis.sql
@@ -1,3 +1,7 @@
+USE [NHS111_Analytics];
+GO
+
+
 ---------------------------------------------------------
 -- Business Question 1
 -- Which NHS organisations receive the highest demand?
@@ -36,13 +40,30 @@ ORDER BY CallsReceived DESC;
 
 SELECT
     REPORTING_PERIOD,
+    DATEFROMPARTS(
+        TRY_CONVERT(INT, RIGHT(REPORTING_PERIOD, 4)),
+        CASE SUBSTRING(REPORTING_PERIOD, 16, 3)
+            WHEN 'JAN' THEN 1
+            WHEN 'FEB' THEN 2
+            WHEN 'MAR' THEN 3
+            WHEN 'APR' THEN 4
+            WHEN 'MAY' THEN 5
+            WHEN 'JUN' THEN 6
+            WHEN 'JUL' THEN 7
+            WHEN 'AUG' THEN 8
+            WHEN 'SEP' THEN 9
+            WHEN 'OCT' THEN 10
+            WHEN 'NOV' THEN 11
+            WHEN 'DEC' THEN 12
+        END,
+        1
+    ) AS ReportingDate,
     SUM(VALUE) AS CallsReceived
 FROM dbo.Fact_IUCADC
 WHERE ITEM_NUMBER = 'A01'
   AND VALUE IS NOT NULL
 GROUP BY REPORTING_PERIOD
-ORDER BY REPORTING_PERIOD;
-
+ORDER BY ReportingDate;
 
 ---------------------------------------------------------
 -- Business Question 4
